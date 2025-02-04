d4acea0e2257dd55a2ad15e37c09f9caca487ba2
IsKillianVincent <pro.killianvincent@gmail.com>
2025-02-04 10:05:01 +0100

    Exercise 2 clean-up

--- Exercice-2/main.py
+++ Exercice-2/main.py
@@ -26,30 +26,40 @@ def main():
     - Remplacement de lettres dans un fichier
     - Affichage du contenu du fichier sous forme de dictionnaire
     """
-    # Question 1 : Validation d'une adresse IP (Try-Except)
-    print_section_title("Question 1 : Validation de l'adresse IP")
-    ip = '123.21' #get_user_ip()
-    print(identify_ip_version(ip))
-
-    # Question 2 - 3 - 4 : Manipulation du fichier
-    print_section_title("Question 2: Manipulation du fichier")
+    # Question 1 : Validation d'une adresse IP
+    print_section_title("Question 1 : Validation d'une adresse IP")
+    try:
+        ip = get_user_ip()
+        print(identify_ip_version(ip))
+    except Exception as e:
+        print(f"Erreur lors de la validation de l'IP : {e}")
+    
+    # Question 2 : Manipulation du fichier - Remplacement de lettres
+    print_section_title("Question 2 : Remplacement de lettres dans un fichier")
     file_path = "./assets/test"
     if not os.path.exists(file_path):
         print(f"Le fichier {file_path} n'a pas été trouvé.")
         return
-
-    # Remplacement des lettres dans le fichier
-    print("Remplacer les lettres Z par X dans le fichier...")
-    replace_letters_in_file(file_path, "z")
-
-    # Lecture du fichier et conversion en dictionnaire
-    print_section_title("Question 3: Lecture du fichier et conversion en dictionnaire")
-    file_dict = file_to_dict(file_path)
-
-    # Affichage du contenu du fichier sous forme de dictionnaire
-    print_section_title("Question 4: Affichage du contenu du fichier sous forme de dictionnaire")
-    display_file_dict(file_dict)
-
+    try:
+        print("Remplacer les lettres 'z' par 'x' dans le fichier...")
+        replace_letters_in_file(file_path, "z")
+    except Exception as e:
+        print(f"Erreur lors du remplacement des lettres : {e}")
+    
+    # Question 3 : Lecture du fichier et conversion en dictionnaire
+    print_section_title("Question 3 : Lecture du fichier et conversion en dictionnaire")
+    try:
+        file_dict = file_to_dict(file_path)
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier : {e}")
+        return
+    
+    # Question 4 : Affichage du contenu du fichier sous forme de dictionnaire
+    print_section_title("Question 4 : Affichage du fichier sous forme de dictionnaire")
+    try:
+        display_file_dict(file_dict)
+    except Exception as e:
+        print(f"Erreur lors de l'affichage du dictionnaire : {e}")
 
 if __name__ == "__main__":
     main()
