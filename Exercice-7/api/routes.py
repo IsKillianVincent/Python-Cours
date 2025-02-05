06e91a65c1923c587a9010a55f2022e327d5aea6
IsKillianVincent <pro.killianvincent@gmail.com>
2025-02-05 12:22:44 +0100

    Fixing business logic

--- Exercice-7/api/routes.py
+++ Exercice-7/api/routes.py
@@ -1,20 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.deck import Deck
 from models.distribution import Distributor
 
 router = APIRouter()
 
+def get_deck() -> Deck:
+    deck = Deck()
+    deck.shuffle()
+    return deck
+
 @router.get("/deck", summary="Récupère un paquet de 52 cartes mélangé")
-def get_deck():
+def get_deck_cards(deck: Deck = Depends(get_deck)):
     """
     Génère et retourne un paquet de 52 cartes mélangé.
     """
-    deck = Deck()
-    deck.shuffle()
     return {"deck": [str(card) for card in deck.cards]}
 
 @router.get("/players/{num_players}", summary="Distribue les cartes entre X joueurs")
-def get_players(num_players: int):
+def get_players(num_players: int, deck: Deck = Depends(get_deck)):
     """
     Distribue les cartes entre un certain nombre de joueurs.
     Retourne un dictionnaire contenant les cartes de chaque joueur.
@@ -22,8 +25,6 @@ def get_players(num_players: int):
     if num_players < 1:
         return {"error": "Le nombre de joueurs doit être positif."}
 
-    deck = Deck()
-    deck.shuffle()
     distributor = Distributor(deck)
 
     try:
